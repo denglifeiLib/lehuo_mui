2bf6f5c8f972688539416446546bdb199bc09c36
denglifeiLib <denglifly3507@163.com>
2020-01-11 18:11:33 +0800

    ad

---
+++ js/common.js
@@ -0,0 +1,30 @@
+//mui初始化
+mui.init({
+	swipeBack: true
+});
+
+//a跳转
+mui('body').on('tap','a',function(){
+	document.location.href=this.href;
+});
+
+
+function copyText(text) {
+	var textarea = document.createElement("input");//创建input对象
+	var currentFocus = document.activeElement;//当前获得焦点的元素
+	document.body.appendChild(textarea);//添加元素
+	textarea.value = text;
+	textarea.focus();
+	if(textarea.setSelectionRange)
+		textarea.setSelectionRange(0, textarea.value.length);//获取光标起始位置到结束位置
+	else
+		textarea.select();
+	try {
+		var flag = document.execCommand("copy");//执行复制
+	} catch(eo) {
+		var flag = false;
+	}
+	document.body.removeChild(textarea);//删除元素
+	currentFocus.focus();
+	return flag;
+}
